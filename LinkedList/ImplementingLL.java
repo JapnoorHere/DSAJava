64ba8d51ca35b0025d7c2252b9c2240f3e7b2e33
Japnoor Singh <109523523+JapnoorHere@users.noreply.github.com>
2023-07-09 22:39:51 +0530

    change

---
+++ LinkedList/ImplementingLL.java
@@ -0,0 +1,16 @@
+class implementingLL{
+    Node head;
+    class Node{
+        String data;
+        Node next;
+        
+        Node(String data){
+            this.data=data;
+            this.next=next;
+        }
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
